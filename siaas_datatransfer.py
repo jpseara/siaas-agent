f5feda6d7dfafd5bed903ba1218c6732a7e9d240
João Pedro Seara <joao.pedro.seara@gmail.com>
2022-10-24 13:30:55 +0100

    Renamed agent module in datatransfer

--- siaas_datatransfer.py
+++ siaas_datatransfer.py
@@ -33,7 +33,7 @@ def upload_agent_data(db_collection=None, last_uploaded_dict={}):
 
     siaas_uid = siaas_aux.get_or_create_unique_system_id()
 
-    all_modules = "agent,config,neighborhood,portscanner"
+    all_modules = "config,neighborhood,platform,portscanner"
     current_dict = siaas_aux.merge_module_dicts(all_modules.split(','))
 
     if (str(current_dict) == str(last_uploaded_dict)) or len(current_dict) == 0:
