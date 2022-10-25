29af4561210dec22e74be4084866b505f24e0b5d
João Pedro Seara <joao.pedro.seara@gmail.com>
2022-10-25 18:59:06 +0100

    Corrected MAC address dict name in neighborhood module

--- siaas_neighborhood.py
+++ siaas_neighborhood.py
@@ -90,7 +90,7 @@ def get_arp_ndp_known_hosts():
 
         dns_entry = ip
         ip_mac_host[ip] = {}
-        ip_mac_host[ip]["macaddress"] = mac
+        ip_mac_host[ip]["mac_address"] = mac
         if len(dns_name) > 0:
             ip_mac_host[ip]["domain_name"] = dns_name
             dns_entry = ip+" ("+dns_name+")"
@@ -152,7 +152,7 @@ def scan_and_print_neighbors(net, interface, timeout=5):
 
         dns_entry = ip
         ip_mac_host[ip] = {}
-        ip_mac_host[ip]["macaddress"] = r.src
+        ip_mac_host[ip]["mac_address"] = r.src
         if len(dns_name) > 0:
             ip_mac_host[ip]["domain_name"] = dns_name
             dns_entry = ip+" ("+dns_name+")"
