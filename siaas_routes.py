35f3cc8ef2e05667eea7fd2ea411e8ae845b5aeb
João Pedro Seara <joao.pedro.seara@gmail.com>
2022-12-14 23:28:49 +0000

    Hide passwords from open API

--- siaas_routes.py
+++ siaas_routes.py
@@ -41,13 +41,9 @@ def siaas_agent():
     else:
         status = "success"
     try:
-        output["config"]["mongo_pwd"] = '*' * \
-            len(output["config"]["mongo_pwd"])
-    except:
-        pass
-    try:
-        output["config"]["api_pwd"] = '*' * \
-            len(output["config"]["api_pwd"])
+        for k in output["config"].keys():
+            if k.endswith("_pwd") or k.endswith("_passwd") or k.endswith("_password"):
+                output["config"][k] = '*' * len(output["config"][k])
     except:
         pass
     return jsonify(
