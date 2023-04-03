c6af86c90f925147a8342cf9d05e5e85e5ac0387
João Pedro Seara <joao.pedro.seara@gmail.com>
2023-04-03 19:00:23 +0100

    Always update vulscan repos

--- siaas_agent_refresh_nmap_scripts_repos.sh
+++ siaas_agent_refresh_nmap_scripts_repos.sh
@@ -16,4 +16,4 @@ echo "Refreshing nmap-vulners repo ..."
 git -C ${NMAP_SCRIPTS_DIR}/nmap-vulners pull 2> /dev/null || (rm -rf ${NMAP_SCRIPTS_DIR}/nmap-vulners && git clone https://github.com/vulnersCom/nmap-vulners.git ${NMAP_SCRIPTS_DIR}/nmap-vulners && ln -fs ${NMAP_SCRIPTS_DIR}/nmap-vulners /usr/share/nmap/scripts/)
 
 echo "Refreshing vulscan repo ..."
-git -C ${NMAP_SCRIPTS_DIR}/vulscan pull 2> /dev/null || (rm -rf ${NMAP_SCRIPTS_DIR}/vulscan && git clone https://github.com/scipag/vulscan.git ${NMAP_SCRIPTS_DIR}/vulscan && cd ${NMAP_SCRIPTS_DIR}/vulscan/utilities/updater/ && chmod +x updateFiles.sh && ./updateFiles.sh && cd - && ln -fs ${NMAP_SCRIPTS_DIR}/vulscan /usr/share/nmap/scripts/)
+(git -C ${NMAP_SCRIPTS_DIR}/vulscan pull 2> /dev/null || (rm -rf ${NMAP_SCRIPTS_DIR}/vulscan && git clone https://github.com/scipag/vulscan.git ${NMAP_SCRIPTS_DIR}/vulscan && ln -fs ${NMAP_SCRIPTS_DIR}/vulscan /usr/share/nmap/scripts/)) && cd ${NMAP_SCRIPTS_DIR}/vulscan/utilities/updater/ && chmod +x updateFiles.sh && ./updateFiles.sh
